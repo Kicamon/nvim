a2c34eecb9313970a3a6a5f97015f679b532d0e4
KicamonIce <1532931259@qq.com>
2023-09-27 18:20:38 +0800

    2023/09/27,18:20

--- init.lua
+++ init.lua
@@ -1,7 +1,4 @@
 require("config.defaults")
 require("config.keymaps")
 require("config.plugins")
-
--- vim.opt.runtimepath:append("$HOME/Documents/project/nvim/SimpleLine.nvim")
--- vim.opt.tabline = '%!v:lua.require\'SimpleLine.tabline\'.tabline()'
--- require("SimpleLine").setup()
+-- require("plugtest")
