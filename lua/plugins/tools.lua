7bd168de2b14c2c5217f67d2a51c5b032c3f7195
KicamonIce <1532931259@qq.com>
2023-10-08 14:06:53 +0800

    simpline

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -8,7 +8,11 @@ return {
     },
 
     config = function()
-      require('competitest').setup()
+      require('competitest').setup({
+        runner_ui = {
+          interface = "split",
+        },
+      })
       vim.keymap.set("n", "rr", "<cmd>CompetiTest run<CR>", { noremap = true })
       vim.keymap.set("n", "ra", "<cmd>CompetiTest add_testcase<CR>", { noremap = true })
       vim.keymap.set("n", "re", "<cmd>CompetiTest edit_testcase<CR>", { noremap = true })
@@ -185,10 +189,10 @@ return {
           left = { ' ', wilder.popupmenu_devicons() },
           right = { ' ', wilder.popupmenu_scrollbar() },
           border = 'rounded',
-          max_height = '75%',    -- max height of the palette
-          min_height = 0,        -- set to the same as 'max_height' for a fixed height window
+          max_height = '75%',      -- max height of the palette
+          min_height = 0,          -- set to the same as 'max_height' for a fixed height window
           prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
-          reverse = 0,           -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+          reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
         })
       ))
       wilder.set_option('pipeline', {
