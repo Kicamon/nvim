560b877cd7cf0aaa78b1976a382097bd5186f459
Cyberczy <109653523+Cyberczy@users.noreply.github.com>
2023-12-10 22:52:45 +0800

    添加对macos以及wsl的剪切板支持

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -71,3 +71,25 @@ vim.g.fts = {
   'html',
   'vim',
 }
+
+if vim.loop.os_uname().sysname == "Darwin" then
+  vim.g.clipboard = {
+    name = "macOS-clipboard",
+    copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
+    paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
+    cache_enabled = 0,
+  }
+elseif vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
