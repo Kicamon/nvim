2e414ffb49f820f821809b4460537a8551d58160
Kicamon <1532931259@qq.com>
2024-06-17 11:53:43 +0800

    epo

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -13,8 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
   require('plugins.ui'),
-  -- require('plugins.epo'),
-  require('plugins.cmp'),
+  require('plugins.epo'),
   require('plugins.lsp'),
   require('plugins.tools'),
 })
