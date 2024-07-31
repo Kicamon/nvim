5f0d6e9f410785e17312df95e23e53f1b9c9a920
Kicamon <kicamonice@gmail.com>
2024-07-31 13:10:58 +0800

    use nvim-snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,12 +5,7 @@ return {
   event = 'LspAttach',
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
-    {
-      'saadparwaiz1/cmp_luasnip',
-      dependencies = {
-        'L3MON4D3/LuaSnip',
-      },
-    },
+    'garymjr/nvim-snippets',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
   },
@@ -20,6 +15,56 @@ return {
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0
           and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+    end
+
+    local function snippet_replace(snippet, fn)
+      return snippet:gsub("%$%b{}", function(m)
+        local n, name = m:match("^%${(%d+):(.+)}$")
+        return n and fn({ n = n, text = name }) or m
+      end) or snippet
+    end
+
+    local function snippet_preview(snippet)
+      local ok, parsed = pcall(function()
+        return vim.lsp._snippet_grammar.parse(snippet)
+      end)
+      return ok and tostring(parsed)
+          or snippet_replace(snippet, function(placeholder)
+            return snippet_preview(placeholder.text)
+          end):gsub("%$0", "")
+    end
+
+    local function snippet_fix(snippet)
+      local texts = {} ---@type table<number, string>
+      return snippet_replace(snippet, function(placeholder)
+        texts[placeholder.n] = texts[placeholder.n] or snippet_preview(placeholder.text)
+        return "${" .. placeholder.n .. ":" .. texts[placeholder.n] .. "}"
+      end)
+    end
+
+    local function expand(snippet)
+      local session = vim.snippet.active() and vim.snippet._session or nil
+
+      local ok, err = pcall(vim.snippet.expand, snippet)
+      if not ok then
+        local fixed = snippet_fix(snippet)
+        ok = pcall(vim.snippet.expand, fixed)
+
+        local msg = ok and 'Failed to parse snippet,\nbut was able to fix it automatically.'
+            or ('Failed to parse snippet.\n' .. err)
+        vim.notify(
+          ([[%s
+            ```%s
+            %s
+            ```]]):format(msg, vim.bo.filetype, snippet),
+          vim.log.levels.WARN
+        )
+      end
+
+      -- Restore top-level session when needed
+      if session then
+        vim.snippet._session = session
+      end
     end
     local kind_icons = {
       Text = " 󰉿 ",
@@ -48,8 +93,7 @@ return {
       Operator = " 󰆕 ",
       TypeParameter = " 󰅲 ",
     }
-    local luasnip = require('luasnip')
-    require('luasnip.loaders.from_vscode').lazy_load({ paths = '~/.config/nvim/snippets' })
+    require('snippets').setup({ search_paths = { vim.fn.stdpath('config') .. '/snippets/' }, })
     local cmp = require('cmp')
     cmp.setup({
       window = {
@@ -76,11 +120,11 @@ return {
       },
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body)
+          expand(args.body)
         end,
       },
       sources = cmp.config.sources({
-        { name = 'luasnip' },
+        { name = 'snippets', priority = 100 },
         { name = 'nvim_lsp' },
         { name = 'path' },
         { name = 'buffer' },
@@ -89,8 +133,10 @@ return {
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
           elseif has_words_before() then
             cmp.complete()
           else
@@ -101,8 +147,10 @@ return {
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
           else
             fallback()
           end
