72354610a8755d1f4eb8d1c8f8c2cebef02c7261
Kicamon <kicamonice@gmail.com>
2024-08-12 00:46:23 +0800

    auto set zsh to sh

--- after/ftplugin/zsh.lua
+++ after/ftplugin/zsh.lua
@@ -1,8 +1,1 @@
-vim.opt_local.expandtab = true
-vim.opt_local.smarttab = true
-vim.opt_local.autoindent = true
-vim.opt_local.smartindent = true
-vim.opt_local.foldmethod = 'syntax'
-vim.opt_local.shiftwidth = 2
-vim.opt_local.softtabstop = 2
-vim.opt_local.tabstop = 2
+vim.cmd.setlocal('filetype=sh')
